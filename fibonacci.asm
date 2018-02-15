0ccde1159694a0f949f963b0a8a80fe9b1ff443a
Guillermo <uriakua@gmail.com>
2018-02-15 17:55:47 -0600

    started stack, created tags and basic structure

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,3 +1,26 @@
 .data
 
 .text
+
+	addi $a0, $zero, 6	# this is n
+	addi $a1, $zero, $a0	#this is used to make the operations
+		
+	jal fibonacci
+	
+main:
+	addi $sp, $sp, -12	#save 3 spaces in the stack
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+   
+	slti $s0, $a0, 2	#jump to base case. investigate base case for fibonacci
+	beq $s0, $zero, fibonacci	
+	
+fibonacci:
+	
+	
+
+exitfibonacci:
+
+
+end:	
